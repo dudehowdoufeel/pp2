2586a568f26b2df926c49c3c8f7689bc177935a0
dudehowdoufeel <156789719+dudehowdoufeel@users.noreply.github.com>
2024-01-23 16:29:50 +0600

    Add files via upload

---
+++ lab1/comments.py
@@ -0,0 +1,13 @@
+#1.Comments in Python are written with a special character, which one?
+
+#This is a comment
+
+
+#2.Use a multiline string to make the a multiline comment:
+
+'''
+
+This is a comment
+written in 
+more than just one line
+'''
