86e08c11eba309337a1eff458123d67f559338eb
tohha <togzhan17.06@gmail.com>
2024-01-28 19:49:03 +0600

    add lab1

---
+++ lab1/data_types.py
@@ -0,0 +1,44 @@
+#1. The following code example would print the data type of x, what data type would that be?
+
+
+x = 5
+print(type(x))
+
+#2. The following code example would print the data type of x, what data type would that be?
+
+
+x = "Hello World"
+print(type(x))
+
+#3. The following code example would print the data type of x, what data type would that be?
+
+
+x = 20.5
+print(type(x))
+
+#4. The following code example would print the data type of x, what data type would that be?
+
+
+x = ["apple", "banana", "cherry"]
+print(type(x))
+
+
+#5. The following code example would print the data type of x, what data type would that be?
+
+
+x = ("apple", "banana", "cherry")
+print(type(x))
+
+
+#6. The following code example would print the data type of x, what data type would that be?
+
+
+x = {"name" : "John", "age" : 36}
+print(type(x))
+
+
+#7. The following code example would print the data type of x, what data type would that be?
+
+
+x = True
+print(type(x))
