d52894910c66066516eb4848fed69b514fd3ddf5
dudehowdoufeel <156789719+dudehowdoufeel@users.noreply.github.com>
2024-01-28 23:02:31 +0600

    Add files via upload

---
+++ labb2/booleans.py
@@ -0,0 +1,43 @@
+#1.The statement below would print a Boolean value, which one?
+
+
+print(10 > 9)
+
+True 
+
+
+
+#2.The statement below would print a Boolean value, which one?
+
+
+print(10 == 9)
+
+False
+
+
+
+#3.The statement below would print a Boolean value, which one?
+
+
+print(10 < 9)
+
+False
+
+
+
+#4.The statement below would print a Boolean value, which one?
+
+
+print(bool("abc"))
+
+True
+
+
+
+
+#5.The statement below would print a Boolean value, which one?
+
+
+print(bool(0))
+
+False
