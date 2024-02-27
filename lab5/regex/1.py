68636e463b622a4d0accb474dae00bf101b76690
tohha <togzhan17.06@gmail.com>
2024-02-27 20:52:51 +0600

    dude

---
+++ lab5/regex/1.py
@@ -0,0 +1,10 @@
+#Write a Python program that matches a string that has an 'a' followed by zero or more 'b''s.
+
+# * - 0 or more occurences (or {0,})
+# + - 1 or more occurences (or {1,})
+# ? - 0 or 1 occurence (or {0,1})
+# {2} - exactly 2 occurences
+# {1,3} - from 1 occurence to 3 occurences
+# {1,} - at least 1 occurence (same as +)
+# {,3} - no more than 3 occurences
+
---
+++ lab5/regex/8.py
@@ -0,0 +1,1 @@
+#Write a Python program to split a string at uppercase letters.
