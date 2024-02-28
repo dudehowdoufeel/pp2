14d6ed71394709b5fac4cd348f288058113a305c
tohha <togzhan17.06@gmail.com>
2024-02-28 21:42:04 +0600

    bdhktycmwhs

--- lab5/regex/1.py
+++ lab5/regex/1.py
@@ -8,3 +8,11 @@
 # {1,} - at least 1 occurence (same as +)
 # {,3} - no more than 3 occurences
 
+import re
+
+with open(r'C:\Users\ASUS\Desktop\pp2\lab5\a.txt', 'r', encoding='utf-8') as file:
+     g = file.read()
+
+match = re.findall(r"ab*", g)
+print(match)
+
