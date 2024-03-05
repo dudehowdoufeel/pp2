c99fde6f80408c66785eb45a8e32b85a08a3c843
tohha <togzhan17.06@gmail.com>
2024-03-05 20:23:31 +0600

    zeloni maski chai

--- lab4/date/1.py
+++ lab4/date/1.py
@@ -3,3 +3,10 @@ today = datetime.datetime.now()
 sub5 = today - datetime.timedelta(days=5)
 print(sub5)
 
+
+'''
+import datetime
+today=datetime.datetime.now()
+five=today + datetime.timedelta(5)
+print(five.strftime("%A"))
+'''
