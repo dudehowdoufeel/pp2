fec7a6f50a3db7ad4958e863c1bb9158ed908249
tohha <togzhan17.06@gmail.com>
2024-04-11 00:57:06 +0600

    gaff

---
+++ lab8_9/racer/game/1.py
@@ -0,0 +1,174 @@
+import pygame
+import random
+from pygame.locals import*
+import time
+import sys
+
+pygame.init()
+
+FSP = 60
+FramePerSec = pygame.time.Clock()
+
+backgroundSound = pygame.mixer.music.load(r"C:\Users\ASUS\Desktop\pp2\lab8\racer\resources\Lecture_G2_Week11_racer_resources_background.wav")
+backgroundSound = pygame.mixer.music.play(-1)
+
+background = pygame.image.load(r"C:\Users\ASUS\Desktop\pp2\lab8\racer\resources\AnimatedStreet.png")
+screen = pygame.display.set_mode((400,600))
+
+speed = 5
+coin_speed = 5
+score = 0
+coins_collected = 0
+
+
+#Setting up Fonts
+font = pygame.font.SysFont("Times New Roman", 60)
+font_small = pygame.font.SysFont("Times New Roman", 20)
+game_over = font.render("Game Over", True, (255,255,255))
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load(r"C:\Users\ASUS\Desktop\pp2\lab8\racer\resources\Player.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (190, 546)
+
+    def move(self):
+        pressed_key = pygame.key.get_pressed()
+
+        if self.rect.left > 0:
+            if pressed_key [K_LEFT]:
+                self.rect.move_ip(-5, 0)
+        if self.rect.right < 400:
+            if pressed_key [K_RIGHT]:
+                self.rect.move_ip(5, 0)
+
+    def collect_coins(self, coins_group):
+        global score, coins_collected, speed
+        collected_coins = pygame.sprite.spritecollide(self, coins_group, True)
+        for coin in collected_coins:
+            if coin.value == 5:
+                score += 5
+            elif coin.value == 10:
+                score += 10
+            elif coin.value == 25:
+                score += 25
+            
+            if coins_collected % N == 0:
+                speed += 0.5
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load(r"C:\Users\ASUS\Desktop\pp2\lab8\racer\resources\Enemy.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randint(40, 360), 0)
+    def move(self):
+         
+        self.rect.move_ip(0, speed)
+        if(self.rect.bottom > 600):
+            self.rect.top = 0
+            self.rect.center = (random.randint(40, 360), 0)
+
+class Coins(pygame.sprite.Sprite):
+    def __init__(self, value, enemies, coin_speed):
+        super().__init__()
+        self.value = value
+        if self.value == 5:
+            self.image = pygame.image.load(r"C:\Users\ASUS\Desktop\pp2\lab8\racer\resources\5coin.png").convert_alpha()
+        elif self.value == 10:
+            self.image = pygame.image.load(r"C:\Users\ASUS\Desktop\pp2\lab8\racer\resources\10coin.png").convert_alpha()
+        elif self.value == 25:
+            self.image = pygame.image.load(r"C:\Users\ASUS\Desktop\pp2\lab8\racer\resources\25coin.png").convert_alpha()
+        else:
+            raise ValueError("Invalid coin value")
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randint(40, 360), 0)
+        self.enemies = enemies
+
+        while pygame.sprite.spritecollideany(self, self.enemies):
+            self.rect.center = (random.randint(40, 360), 0)
+
+    def move(self): 
+        self.rect.move_ip(0, coin_speed)
+        if self.rect.bottom > 600:
+            self.rect.top = 0
+            self.rect.center = (random.randint(40, 360), 0)
+
+            while pygame.sprite.spritecollideany(self, self.enemies):
+                self.rect.center = (random.randint(40, 360), 0)
+
+        
+
+# Initialize Player and Enemy
+p1 = Player()
+e1 = Enemy()
+
+# Initialize enemies group and add enemy to it
+enemies = pygame.sprite.Group()
+enemies.add(e1)
+
+
+coins = pygame.sprite.Group()
+c1 = Coins(5, enemies, coin_speed=3)
+coins.add(c1)
+
+# Create all sprites group and add Player, Enemy, and Coins to it
+all_sprites = pygame.sprite.Group()
+all_sprites.add(p1)
+all_sprites.add(e1)
+all_sprites.add(c1)
+
+inc_speed = pygame.USEREVENT + 1
+pygame.time.set_timer(inc_speed, 1000)
+
+N = 10
+
+done = False
+while not done:
+    for event in pygame.event.get():
+        
+        if event.type == inc_speed:
+        # Create a new instance of the Coins class and add it to the coins group and all_sprites group
+          new_coin = Coins(random.choice([5, 10, 25]), enemies, coin_speed=3)  # Randomly choose a coin value
+          coins.add(new_coin)
+          all_sprites.add(new_coin)
+
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        
+    
+    screen.blit(background, (0,0))
+    scores = font_small.render(str(score), True, (0, 0, 0))
+    screen.blit(scores, (10,10))
+
+
+    for entity in all_sprites:
+        entity.move()
+        screen.blit(entity.image, entity.rect)
+
+    p1.collect_coins(coins)
+
+
+    if pygame.sprite.spritecollideany(p1, enemies):
+        backgroundSound = pygame.mixer.music.stop()
+        crash = pygame.mixer.Sound(r"C:\Users\ASUS\Desktop\pp2\lab8\racer\resources\Lecture_G2_Week11_racer_resources_crash.wav")
+        crash.play(1)
+        time.sleep(1)
+        
+
+        screen.fill((255,0,0))
+
+        scores = font.render("Score: " + str(score), True, (255, 255, 255))
+        screen.blit(scores, (70,350))
+        screen.blit(game_over, (50, 250))
+        pygame.display.update()
+        
+
+
+    pygame.display.update()
+    FramePerSec.tick(FSP)
+
+
---
+++ lab8_9/snake/1.py
@@ -0,0 +1,114 @@
+import pygame
+import sys
+
+pygame.init()
+
+# color palette
+colorBLACK = (0, 0, 0)
+colorGRAY = (200, 200, 200)
+colorWHITE = (255, 255, 255)
+colorRED = (255, 0, 0)
+colorGREEN = (0, 255, 0)
+colorBLUE = (0, 0, 255)
+colorYELLOW = (255, 255, 0)
+
+WIDTH = 600
+HEIGHT = 600
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+FPS = 5
+clock = pygame.time.Clock()
+
+CELL = 30
+
+def draw_grid():
+    for i in range(HEIGHT // CELL):
+        for j in range(WIDTH // CELL):
+            pygame.draw.rect(screen, colorGRAY, (i * CELL, j * CELL, CELL, CELL), 1)
+
+def draw_grid_chess():
+    colors = [colorWHITE, colorGRAY]
+
+    for i in range(HEIGHT // CELL):
+        for j in range(WIDTH // CELL):
+            pygame.draw.rect(screen, colors[(i + j) % 2], (i * CELL, j * CELL, CELL, CELL))
+
+class Point:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+
+class Snake:
+    def __init__(self):
+        self.body = [Point(10, 11), Point(10, 12), Point(10, 13)]
+        self.dx = 1
+        self.dy = 0
+
+    def move(self):
+        for i in range(len(self.body) - 1, 0, -1):
+            self.body[i].x = self.body[i - 1].x
+            self.body[i].y = self.body[i - 1].y
+        
+        self.body[0].x += self.dx
+        self.body[0].y += self.dy
+
+    def draw(self):
+        head = self.body[0]
+        pygame.draw.rect(screen, colorRED, (head.x * CELL, head.y * CELL, CELL, CELL))
+        for segment in self.body[1:]:
+            pygame.draw.rect(screen, colorYELLOW, (segment.x * CELL, segment.y * CELL, CELL, CELL))
+
+    def check_collision(self, food):
+        head = self.body[0]
+        if head.x == food.pos.x and head.y == food.pos.y:
+            self.body.append(Point(head.x, head.y))
+            return True
+        return False
+
+class Food:
+    def __init__(self):
+        self.pos = Point(15, 15)
+
+    def move(self):
+        pass
+
+    def draw(self):
+        pygame.draw.rect(screen, colorGREEN, (self.pos.x * CELL, self.pos.y * CELL, CELL, CELL))
+
+done = False
+
+snake = Snake()
+food = Food()
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                snake.dx = 1
+                snake.dy = 0
+            if event.key == pygame.K_LEFT:
+                snake.dx = -1
+                snake.dy = 0
+            if event.key == pygame.K_UP:
+                snake.dx = 0
+                snake.dy = -1
+            if event.key == pygame.K_DOWN:
+                snake.dx = 0
+                snake.dy = 1
+
+    draw_grid_chess()
+
+    snake.move()
+    snake.draw()
+
+    food.draw()
+
+    if snake.check_collision(food):
+        print("Got food!")
+    
+    pygame.display.flip()
+    clock.tick(FPS)
